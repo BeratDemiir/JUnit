97941f2da06f1ce8eb0d3f60fbde7ddc0ec7a9bf
Berat Demir <brtdmr2726@gmail.com>
2023-09-25 22:07:34 +0300

    25.09.23

---
+++ src/test/java/day04_Maven/C01_Maven.java
@@ -0,0 +1,24 @@
+package day04_Maven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Maven {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        // https://www.amazon.com/ sayfasina gidelim
+        // arama kutusunu locate edelim
+        // “Samsung headphones” ile arama yapalim
+        // Bulunan sonuc sayisini yazdiralim
+        // Ilk urunu tiklayalim
+        // Sayfadaki tum basliklari yazdiralim
+    }
+}
