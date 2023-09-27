9be8b49a67f3d89f628218995dd72971a4c25238
Berat Demir <brtdmr2726@gmail.com>
2023-09-27 21:16:43 +0300

    27.09.23

---
+++ src/test/java/day05_JUnit/C05_BAExample.java
@@ -0,0 +1,50 @@
+package day05_JUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C05_BAExample {
+
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.close();
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+        //  Verilen web sayfasına gidin.
+        //  https://the-internet.herokuapp.com/checkboxes
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        //  Checkbox1 ve checkbox2 elementlerini locate edin
+       WebElement checkbbox1 = driver.findElement(By.xpath("(//*[@type='checkbox'])[1]"));
+       WebElement checkbbox2 = driver.findElement(By.xpath("(//*[@type='checkbox'])[2]"));
+
+        //  Checkbox1 seçili değilse onay kutusunu tıklayın
+        Thread.sleep(3000);
+        if (!checkbbox1.isSelected()){  //Eger secili degilse
+            checkbbox1.click();
+        }
+        //  Checkbox2 seçili değilse onay kutusunu tıklayın
+        if (!checkbbox2.isSelected()){  //Eger secili degilse
+            checkbbox2.click();
+        }
+    }
+}
