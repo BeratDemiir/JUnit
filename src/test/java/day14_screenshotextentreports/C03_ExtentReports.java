d07d0459a65829acb865637c34456ffe64b3f59b
Berat Demir <brtdmr2726@gmail.com>
2023-10-18 20:54:56 +0300

    18.10.23

---
+++ src/test/java/day14_screenshotextentreports/C03_ExtentReports.java
@@ -0,0 +1,50 @@
+package day14_screenshotextentreports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.junit.Test;
+import utilities.TestBase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C03_ExtentReports extends TestBase {
+
+    //  HATIRLAMAMIZ GEREKEN 3 CLASS
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+    @Test
+    public void extentReportsTest() {
+
+        // Rapor yolu
+        String currenTime = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String path = System.getProperty("user.dir")+ "/test-output/Screenshots/"+currenTime+"image.png";
+
+        // HTML raporu oluşturma yolu
+        extentHtmlReporter = new ExtentHtmlReporter(path);
+
+        // Yapılandırma ile tüm raporları oluşturmak için kapsam raporları nesnesi oluşturma
+        extentReports = new ExtentReports();
+
+        // RAPORU CUSTOMIZE EDEBILIRIZ
+        extentReports.setSystemInfo("Test Environment", "Regression");
+        extentReports.setSystemInfo("Application", "TechPro Education");
+        extentReports.setSystemInfo("Browser", "Chrome");
+        extentReports.setSystemInfo("Takim", "Lions");
+        extentReports.setSystemInfo("Epic", "Odeme Sayfasi");
+        extentReports.setSystemInfo("Sprint Numarasi", "Sprint-145");
+        extentReports.setSystemInfo("QA", "Berat");
+
+
+        // EXTRA RAPOR ISMI VE DOCUMAN ISMI EKLEYEBILIRIZ
+        extentHtmlReporter.config().setDocumentTitle("TechProEd Extent Reports");
+        extentHtmlReporter.config().setReportName("Regression Test Sonucu");
+
+        // RAPOR AYARLARI BITTI
+
+        extentReports.attachReporter(extentHtmlReporter);
+    }
+}
