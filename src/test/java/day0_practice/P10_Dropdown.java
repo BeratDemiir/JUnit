123bd05571368074d74bef29538f744f0439c767
Berat Demir <brtdmr2726@gmail.com>
2024-02-21 15:27:38 +0300

    21.02.24

--- src/test/java/day0_practice/P10_Dropdown.java
+++ src/test/java/day0_practice/P10_Dropdown.java
@@ -17,13 +17,13 @@ public class P10_Dropdown extends TestBase {
         driver.get(" https://the-internet.herokuapp.com/dropdown");
 
         // Index kullanarak option 1 secin ve sectiginiz option'u yazdirin.
-      WebElement ddm = driver.findElement(By.id("dropdown"));
+        WebElement ddm = driver.findElement(By.id("dropdown"));
 
         Select select = new Select(ddm);
 
         select.selectByIndex(1);
 
-       String option1 = select.getFirstSelectedOption().getText();
+        String option1 = select.getFirstSelectedOption().getText();
         System.out.println(option1);
 
         Thread.sleep(3000);
@@ -32,28 +32,28 @@ public class P10_Dropdown extends TestBase {
 
         select.selectByValue("2");
         Thread.sleep(3000);
-       String option2 = select.getFirstSelectedOption().getText();
+        String option2 = select.getFirstSelectedOption().getText();
         System.out.println(option2);
 
         // Visible text  kullanarak option 1 secin ve sectiginiz option'u yazdirin.
 
         select.selectByVisibleText("Option 1");
         Thread.sleep(3000);
-       String option3 = select.getFirstSelectedOption().getText();
+        String option3 = select.getFirstSelectedOption().getText();
         System.out.println(option3);
 
         // Tum optionlari yazdirin
-       List<WebElement> tumoptionlar = select.getOptions();
+        List<WebElement> tumoptionlar = select.getOptions();
 
-       int sayac = 1;
-       for (WebElement each : tumoptionlar){
-           System.out.println(sayac + ". Option " + each.getText());
-           sayac++;
-       }
+        int sayac = 1;
+        for (WebElement each : tumoptionlar) {
+            System.out.println(sayac + ". Option " + each.getText());
+            sayac++;
+        }
 
         // dropdown'daki optionlarin toplamsayisinin 3'e esit oldugunu test edin.
-        int expected =3;
-       int actual = tumoptionlar.size();
-        Assert.assertEquals(expected,actual);
+        int expected = 3;
+        int actual = tumoptionlar.size();
+        Assert.assertEquals(expected, actual);
     }
 }
