c091ff482ad8e21ce6eb1c0f1f8b99234d2f5015
Berat Demir <brtdmr2726@gmail.com>
2024-03-01 08:38:42 +0300

    01.03.24

--- src/test/java/day05_JUnit/C04_BeforeAfterClass.java
+++ src/test/java/day05_JUnit/C04_BeforeAfterClass.java
@@ -10,40 +10,46 @@ import java.time.Duration;
 public class C04_BeforeAfterClass {
 
     static WebDriver driver;
+
     /*
          @BeforeClass ve @AfterClass notasyonlari sadece static method'lar icin calisir.
          @BeforeClass ve @AfterClass kullanirsak olusturdugumuz @Test methodlarin hepsini ayni anda calistirip en son @AfterClass'i calistiririz.
          Ama sadece @Before ve @After kullanirsak her test icin @before ve @after'i kullanilir.
      */
     @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
         System.out.println("Butun test'ler den once calisti.");
 
     }
+
     @AfterClass
-    public static void tearDown(){
+    public static void tearDown() {
         System.out.println("Butun test'ler den sonra calisti.");
     }
+
     @Before
-    public void setUp01(){
+    public void setUp01() {
         System.out.println("Her testen once calisir");
     }
+
     @After
-    public void tearDown01(){
+    public void tearDown01() {
         System.out.println("Her testen sonra calisir");
     }
 
     @Test
-    public void test01(){
+    public void test01() {
         System.out.println("Ilk test");
     }
+
     @Test
-    public void test02(){
+    public void test02() {
         System.out.println("Ikinci test");
     }
+
     @Test
     @Ignore // Bir testi calistirmak istemedigimiz zaman kullanilir.
-    public void test03(){
+    public void test03() {
         System.out.println("Ucuncu test");
     }
 }
